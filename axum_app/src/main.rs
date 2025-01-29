2769f360720c58e69e729286e0aae90728d9ec90
apetbrz <apetbrz@gmail.com>
2025-01-29 08:02:30 -0500

    Added a password hashing endpoint to axum and express apps

--- axum_app/src/main.rs
+++ axum_app/src/main.rs
@@ -1,21 +1,39 @@
 use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    Json, Router,
+    body::Body, http::StatusCode, response::Response, routing::{get, post}, Json, Router
 };
 use serde::{Deserialize, Serialize};
+use bcrypt;
 
 #[tokio::main]
 async fn main() {
 
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/hash", post(hash));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 
 }
 
-async fn root() -> &'static str {
-    "Hello, world!"
+#[derive(Serialize, Deserialize)]
+struct Message {
+    message: String
+}
+async fn root() -> axum::Json<Message> {
+    Json(Message{message: "Hello World".into()})
+}
+
+#[derive(Serialize, Deserialize)]
+struct HashMessage {
+    password: String
+}
+
+async fn hash(Json(posted_message): Json<HashMessage>) -> axum::Json<HashMessage> {
+
+    let Ok(hashed_pw) = bcrypt::hash(posted_message.password, 8) else {
+        return Json(HashMessage{password: "Error".into()})
+    };
+
+    Json(HashMessage{password: hashed_pw})
 }
--- express_app/index.js
+++ express_app/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 
 const port = 3001;
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.json({message: "Hello World"})
 });
+
+app.post("/hash", async (req, res) => {
+    let msg = req.body;
+    let hashed_pw = await bcrypt.hash(msg.password, 8);
+
+    res.json({password: hashed_pw});
+})
 
 app.listen(port, () => {
     console.log('Server started on port ' + port);
