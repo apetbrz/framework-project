a16ceae5af8b20ac5bf86fef1dc88e4a9b59c67b
apetbrz <apetbrz@gmail.com>
2025-02-10 10:59:27 -0500

    fixed dotnet url, removed Models (will make minimal api)

--- dotnet_app/Models/PasswordHash.cs
+++
@@ -1,6 +0,0 @@
-namespace dotnet_app.Models;
-
-public class PasswordHash{
-    public required string Password { get; set; }
-    public Guid Id{ get; set; }
-}
--- dotnet_app/Program.cs
+++ dotnet_app/Program.cs
@@ -10,6 +10,7 @@ public class App{
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.WebHost.UseUrls("http://0.0.0.0:5217");
 
         var app = builder.Build();
 
