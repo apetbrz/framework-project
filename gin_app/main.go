46f5c4a8d5a5791cce9a25b12d30ea65e512d169
apetbrz <apetbrz@gmail.com>
2025-02-10 11:52:48 -0500

    built go hello and hash endpoints

--- gin_app/main.go
+++ gin_app/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type PasswordMsg struct {
+	Password string `json:"password" binding:"required"`
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "Hello, world!",
 		})
 	})
+
+	r.POST("/hash", func(c *gin.Context) {
+		var password PasswordMsg
+		fmt.Println("incoming: ", c.Request.Body)
+		c.BindJSON(&password)
+		fmt.Println(password.Password)
+		hash, _ := bcrypt.GenerateFromPassword([]byte(password.Password), 8)
+		fmt.Println(string(hash))
+		c.JSON(http.StatusOK, &PasswordMsg{Password: string(hash)})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
