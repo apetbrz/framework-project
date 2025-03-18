f0c4b57d72bb66e944a0069bb77a3528055536fb
apetbrz <apetbrz@gmail.com>
2025-03-18 11:26:58 -0400

    Began work on ASP.NET dynamic page

--- dotnet_app/src/Program.cs
+++ dotnet_app/src/Program.cs
@@ -7,29 +7,24 @@ public class App{
     public static void Main(string[] args){
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
         builder.WebHost.UseUrls("http://0.0.0.0:5217");
         //builder.Host.UseContentRoot(Path.Combine(builder.Environment.ContentRootPath, "wwwroot"));
+
+        builder.Services.AddRazorPages();
 
         var app = builder.Build();
 
         app.UseStaticFiles();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
         app.MapGet("/hello", (HttpContext httpContext) => {
             return new Message(message: "Hello, world!");
         });
 
-        app.UseRewriter(new RewriteOptions().AddRewrite("/static", "/dotnet_static_page.html", true));
+        var rewrittenRoutes = new RewriteOptions();
+        rewrittenRoutes.AddRewrite("/static", "/dotnet_static_page.html", true);
+        rewrittenRoutes.AddRewrite("/dynamic", "/Dynamic/DotnetDynamicPage", true);
+
+        app.UseRewriter(rewrittenRoutes);
 
         app.MapPost("/hash", (PasswordMessage msg) => {
             string hash = BCrypt.HashPassword(msg.password);
