37020403b130486bfbd0100a9f37aec673e1cd38
arthur petroff pi <apetbrz@gmail.com>
2025-03-31 09:30:27 -0400

    added getmetric script to check data points in output files

---
+++ test_scripts/getmetric.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+filename=$1
+metric=$2
+
+if [[ filename = '-h' ]]
+then
+
+echo "
+~=[ Arthur's Data Value Extrator ]=~
+   for Spring 2025 Senior Project
+
+Usage:
+./getmetric.sh [filename] [metric]
+
+Available Metrics:
+latency_avg
+latency_max
+latency_min
+latency_std_deviation
+requests_avg
+requests_total
+transfer_rate
+transfer_total
+"
+
+exit
+
+fi
+
+jq -r "{\"metric\":\"$metric\",\"data\":[.tests[] | {\"name\":.name,\"value\":.data.$metric}]}" $filename
