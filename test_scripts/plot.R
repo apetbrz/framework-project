5fe8065d48cc0801df5a000f66b15283fe5a42fa
arthur petroff pi <apetbrz@gmail.com>
2025-04-08 10:25:54 -0400

    split graphs between low and high connection counts, added value labels

--- test_scripts/plot.R
+++ test_scripts/plot.R
@@ -3,8 +3,9 @@
 library("jsonlite")
 library("ggplot2")
 library("collapse")
+library("ggrepel")
 
-thread_counts <- c(1, 4, 16, 64, 256, 1024, 2048, 4096)
+thread_counts <- list("low" = c(1, 4, 16, 64), "high" = c(256, 1024, 2048, 4096))
 
 print("Reading from freshdata/")
 print("Please ensure only one file from each test is included")
@@ -27,83 +28,98 @@ gin_data <- lapply(gin_files, fromJSON)
 data <- list("axum" = axum_data, "dotnet" = dotnet_data, "express" = express_data, "gin" = gin_data)
 
 # helper functions:
-make_four <- function(el) {rep(el,4)}
+make_multiple <- function(el, num) {rep(el,num)}
 make_string <- function(num) {paste(num, "", sep="")}
 
 # the four endpoints (will make four separate graphs)
 endpoint <- c("hello", "static", "dynamic", "hash")
 
 # x axis: 1 1 1 1 4 4 4 4 16 16 16 16 ...
-connections <- unlist(lapply(lapply(thread_counts, make_string), make_four))
+connections <- unlist(lapply(lapply(thread_counts, make_string), make_multiple))
 
 # x axis: axum .net express gin axum .net express gin ...
-framework <- rep(c("Axum (Rust)", "ASP.NET (C#)", "Express (JavaScript)", "Gin (Go)"), 8)
+framework <- c("Axum (Rust)", "ASP.NET (C#)", "Express (JavaScript)", "Gin (Go)")
 
 # for each endpoint (each graph .png)
 for(endpnt in endpoint){
   
-  # initialize values list
-  # y axis, latency avgs
-  value <- c()
-
+  tier_index <- 0
   # for each connection count, in order
-  for(conn in thread_counts){
-
-    # for each framework, in order
-    for(frmwrk in data){
+  for(tier in thread_counts){
+    tier_index <- tier_index + 1
+    print(endpnt)
+    print(tier)
+    # initialize values list
+    # y axis, latency avgs
+    value <- c()
+    std_dev <- c()
+    for(conn in tier){
+      # for each framework, in order
+      for(frmwrk in data){
       
-      # get the appropriate data for the current connection count
-      current_data <- frmwrk[[
-        # regex string: checks for the connection count in between _ and -,
-        # with zero or more leading zeroes
-        # grep returns index that matches on 'test' (testname)
-        grep(paste("_[0]*",conn,"-",sep=""),
-          # get_elem returns a list of the test names
-          get_elem(frmwrk, 'test'))
-      ]]
+        # get the appropriate data for the current connection count
+        current_data <- frmwrk[[
+          # regex string: checks for the connection count in between _ and -,
+          # with zero or more leading zeroes
+          # grep returns index that matches on 'test' (testname)
+          grep(paste("_[0]*",conn,"-",sep=""),
+            # get_elem returns a list of the test names
+            get_elem(frmwrk, 'test'))
+        ]]
 
-      data_point <- current_data[['tests']][[endpnt]][['data']][['latency_avg']]
+        data_point <- current_data[['tests']][[endpnt]][['data']][['latency_avg']]
+        std_dev_data_point <- current_data[['tests']][[endpnt]][['data']][['latency_std_deviation']]
 
-      if(is.null(data_point)){
-        stop("NULL value found! Data is bad!")
-      }
+        if(is.null(data_point)){
+          stop("NULL value found! Data is bad!")
+        }
 
-      # append data to values list
-      value <- append(value, data_point)
+        # append data to values list
+        value <- append(value, data_point)
+        std_dev <- append(std_dev, std_dev_data_point)
 
-    }# end frmwrk
-  }# end conn
+      }# end frmwrk
+    }# end conn
   
-  print(value)
-  print(connections)
+    connections <- unlist(lapply(lapply(tier, make_string), make_multiple, num=length(framework)))
+    framework_axis <- rep(framework,length(tier))
 
-  # store in a dataframe
-  df <- data.frame(connections, framework, value)
+    # store in a dataframe
+    df <- data.frame(connections, framework_axis, value, std_dev)
 
-  # print the df, to make sure everything is okay
-  print(df)
+    # print the df, to make sure everything is okay
+    print(df)
 
-  # create and save the png
-  ggsave(
-         # file name uses endpoint name
-         paste("graphs/",endpnt,"_data.png", sep=""), 
+    # create and save the png
+    ggsave(
+           # file name uses endpoint name
+           paste("graphs/",endpnt,"_data_",names(thread_counts)[tier_index],".png", sep=""), 
 
-         # plot the df
-         ggplot(df,
-                # color based on framework, y=data, x=conn count
-                aes(fill=framework, y=value, x=connections)
-                ) + 
-         # bar graph
-         geom_bar(position="dodge", stat="identity") + 
-         # title based on endpoint
-         ggtitle(paste("/",endpnt,"\nEndpoint",sep="")) +
-         # x axis label
-         xlab("Concurrent Connection Count") +
-         # y axis label
-         ylab("Average Response Latency (ms)") +
-         # order x axis based on connections list (in order)
-         scale_x_discrete(limits=connections) +
-         # y axis tick every 100ms
-         scale_y_continuous(breaks=seq(0,4000,100))
-  )
+           # plot the df
+           ggplot(df,
+                  # color based on framework, y=data, x=conn count
+                  aes(fill=framework_axis, y=value, x=connections)
+                  ) + 
+           # bar graph
+           geom_bar(position=position_dodge(3.1), stat="identity", width=3) + 
+           # with std deviation bars
+           geom_errorbar(aes(x=connections, ymin=value-std_dev,ymax=value+std_dev), position=position_dodge(3.1), linewidth=0.5) +
+           # with labels
+           geom_label_repel(aes(label=round(value,2)), position=position_dodge(3.1), size=2) + 
+           # title based on endpoint
+           ggtitle(paste("/",endpnt,"\nEndpoint",sep="")) +
+           # x axis label
+           xlab("Concurrent Connection Count") +
+           # y axis label
+           ylab("Average Response Latency (ms)") +
+           # legend label
+           labs(fill="Framework") +
+           # y limit: 0 and max
+           ylim(0,max(value)*1.1) + 
+           # order x axis based on connections list (in order)
+           scale_x_discrete(limits=connections) #+
+           # y axis tick every 100ms
+           # scale_y_continuous(breaks=seq(0,max(value)+100,if(tier_index==1) 25 else 200)
+    )
+  }# end tier
 }
