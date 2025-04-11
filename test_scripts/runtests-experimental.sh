a0ed58d781872ebb82773e567e153646ba5f63b1
Arthur Petroff Desktop <apetbrz@gmail.com>
2025-04-11 17:59:31 -0400

    removed default hostname from runtests scripts

--- test_scripts/runtests-experimental.sh
+++ test_scripts/runtests-experimental.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-if [[ $1 = '-h' || $1 = '--help' || $1 = '-?' ]]
-then
+if [[ $1 = '-h' || $1 = '--help' || $1 = '-?' ]]; then
 
-echo $"
+  echo $"
 ~=[ Arthur's Automated Test System ]=~
     for Spring 2025 Senior Project
 
+Experimental version depends on https://github.com/apetbrz/rewrk
+Clone and build it, and link the binary as 'rewrk-experimental' in your $PATH
+
 Usage:
-./runtests.sh [framework target] {duration sec (60)} {hostname (castelo.lan)} {threadcount}
+./runtests-experimental.sh [framework target] [hostname] {duration (s)} {threadcount}
+
+Omitting duration defaults to 60s
+Omitting threadcount runs 1, 4, 16, 64, 256, 1024, 2048.
+Threadcount input only supports one at a time.
 "
 
-exit
+  exit
 
 fi
 
@@ -24,91 +30,89 @@ default_threads=(1 4 16 64 256 1024 2048 4096)
 default_duration=60
 
 target=$1
-duration=${2:-${default_duration}}
-host=$"${3:-"castelo.lan"}:"
+host=$2
+duration=${3:-${default_duration}}
 threads=($4)
 threads=${threads:-${default_threads[@]}}
 
-if [ ! -z "$( find freshdata -maxdepth 1 -name "data_$target*" )" ]
-then
+if [ ! -z "$(find freshdata -maxdepth 1 -name "data_$target*")" ]; then
 
-echo "Data for $target found inside freshdata/
+  echo "Data for $target found inside freshdata/
 Please move it!
 Exiting..."
 
-exit
+  exit
 
 fi
 
 case $target in
 
-  "axum")
-    host="${host}${axum_port}"
-    ;;
-  "dotnet")
-    host="${host}${dotnet_port}"
-    ;;
-  "express")
-    host="${host}${express_port}"
-    ;;
-  "gin")
-    host="${host}${gin_port}"
-    ;;
-  *)
-    echo "invalid framework target: $(target)"
-    return
-    ;;
+"axum")
+  host="${host}${axum_port}"
+  ;;
+"dotnet")
+  host="${host}${dotnet_port}"
+  ;;
+"express")
+  host="${host}${express_port}"
+  ;;
+"gin")
+  host="${host}${gin_port}"
+  ;;
+*)
+  echo "invalid framework target: $(target)"
+  return
+  ;;
 
 esac
 
-for threadcount in $threads
-do
+for threadcount in $threads; do
 
-printf -v formatted_threadcount "%04d" $threadcount
+  printf -v formatted_threadcount "%04d" $threadcount
 
-filename="data_${target}_${formatted_threadcount}-conn_$(date "+%Y-%m-%d_%H-%M-%S.json")"
+  filename="data_${target}_${formatted_threadcount}-conn_$(date "+%Y-%m-%d_%H-%M-%S.json")"
 
-touch "freshdata/$filename"
+  touch "freshdata/$filename"
 
-echo "
+  echo "
 Starting $filename"
 
-hello_starttime="$(date "+%T")"
-echo "/hello start: $hello_starttime"
-hello_output="$(./awsts-experimental.sh $host hello $threadcount $duration --json)"
+  hello_starttime="$(date "+%T")"
+  echo "/hello start: $hello_starttime"
+  hello_output="$(./awsts-experimental.sh $host hello $threadcount $duration --json)"
 
-static_starttime="$(date "+%T")"
-echo "/static start: $static_starttime"
-static_output="$(./awsts-experimental.sh $host static $threadcount $duration --json)"
+  static_starttime="$(date "+%T")"
+  echo "/static start: $static_starttime"
+  static_output="$(./awsts-experimental.sh $host static $threadcount $duration --json)"
 
-dynamic_starttime="$(date "+%T")"
-echo "/dynamic start: $dynamic_starttime"
-dynamic_output="$(./awsts-experimental.sh $host dynamic $threadcount $duration --json)"
+  dynamic_starttime="$(date "+%T")"
+  echo "/dynamic start: $dynamic_starttime"
+  dynamic_output="$(./awsts-experimental.sh $host dynamic $threadcount $duration --json)"
 
-hash_starttime="$(date "+%T")"
-echo "/hash start: $hash_starttime"
-hash_output="$(./awsts-experimental.sh $host hash $threadcount $duration this_is_a_very_long_password --json)"
+  hash_starttime="$(date "+%T")"
+  echo "/hash start: $hash_starttime"
+  hash_output="$(./awsts-experimental.sh $host hash $threadcount $duration this_is_a_very_long_password --json)"
 
-jq --null-input \
-   --arg testname "${target}_${threadcount}-conn" \
-   --arg testdate "$(date "+%d-%m-%Y")" \
-   --arg hellotime $hello_starttime \
-   --argjson hellodata $hello_output \
-   --arg statictime $static_starttime \
-   --argjson staticdata $static_output \
-   --arg dynamictime $dynamic_starttime \
-   --argjson dynamicdata $dynamic_output \
-   --arg hashtime $hash_starttime \
-   --argjson hashdata $hash_output \
-  '{"test":$testname,"date":$testdate,
+  jq --null-input \
+    --arg testname "${target}_${threadcount}-conn" \
+    --arg testdate "$(date "+%d-%m-%Y")" \
+    --arg hellotime $hello_starttime \
+    --argjson hellodata $hello_output \
+    --arg statictime $static_starttime \
+    --argjson staticdata $static_output \
+    --arg dynamictime $dynamic_starttime \
+    --argjson dynamicdata $dynamic_output \
+    --arg hashtime $hash_starttime \
+    --argjson hashdata $hash_output \
+    '{"test":$testname,"date":$testdate,
     "tests":{
      "hello":{"name":"hello","time":$hellotime,"data":$hellodata},
      "static":{"name":"static","time":$statictime,"data":$staticdata},
      "dynamic":{"name":"dynamic","time":$dynamictime,"data":$dynamicdata},
      "hash":{"name":"hash","time":$hashtime,"data":$hashdata}
     }
-   }' > "freshdata/$filename"
+   }' >"freshdata/$filename"
 
-echo "Finished $filename"
+  echo "Finished $filename"
 
 done
